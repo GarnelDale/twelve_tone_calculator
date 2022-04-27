ee881c7550d78029a35a0ea2d9e5201bef4bfd86
Torin Bolander <bol19001@byui.edu>
2022-04-27 15:51:00 -0400

    files for React app added

---
+++ twelve-tone-calculator/src/twelve_tone_UI.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './twelve_tone_script.js';
+import './twelve_tone_style.css';
+
+const conversion = {
+    0: 'C',
+    1: 'C\u226F/D\u226D',
+    2: 'D',
+    3: 'D\u226F/E\u226D',
+    4: 'E',
+    5: 'F',
+    6: 'F\u226F/G\u226D',
+    7: 'G',
+    8: 'G\u226F/A\u226D',
+    9: 'A',
+    10: 'A\u226F/B\u226D',
+    11: 'B'
+};
